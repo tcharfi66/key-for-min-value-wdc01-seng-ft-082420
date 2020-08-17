ab3fb105c916e3e23f722203416b4c9f760430f4
Tej Charfi <charfi@usc.edu>
2020-08-17 21:39:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,11 @@ def key_for_min_value(name_hash)
   
   name_hash.each do |k,v|
     if (min_val = nil)
-      min_val=v 
-      key=k
-    else if(v< min_val)
-      min_val=v 
-      key=k
+      min_val= v 
+      key= k
+    else if(v < min_val)
+      min_val= v 
+      key= k
     end
   end
   
