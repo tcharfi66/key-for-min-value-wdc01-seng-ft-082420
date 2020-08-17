902f2b17f7ea1feeb275cf85e161a98de0d13fe4
Tej Charfi <charfi@usc.edu>
2020-08-17 21:36:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  if (name_hash.empty?)
+          return nil
+  else 
+      name_hash.each do |k,v|
+          smallest_val = v
+          name = k
+          if (v < smallest_val)
+            smallest_val = v
+          end
+      end
+  end
+  
+  name_hash[smallest_val]
+  
 end
